e5389986efafad48f420d5cdd8fd42181d96852c
gcq <gcquiroga@gmail.com>
2013-12-20 02:17:23 +0100

    Basic main.py rework

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 import argparse
-import time
+import logging
 import concurrent.futures as futures
-from importlib import machinery
+import importlib
+import plugins
+import sys
 
-#--------------Function definition--------------------
 
-def cleanup():
-	filelist = glob.glob("*.py?")
-	for f in filelist:
-		os.remove(f)
-        
-def load_plugin(name):
-    mod = __import__("plugin_%s" % name)
-    return mod
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+stream = logging.StreamHandler(sys.stdout)
+stream.setFormatter(logging.Formatter("[{levelname}] -> {message}", style="{"))
+logger.addHandler(stream)
 
-def load_plugins(config_file):
-    plugins = []
-    pnames = []
-    with open(config_file, "r") as fh:
-        for line in fh:
-            line = line.strip()
-            if line.startswith("#") or line == "":
-                continue
-            pnames.append(line)
-        pnames.sort()
-        for i in pnames:
-            plugins.append(load_plugin(i))
-    return plugins
-
-#-----------------Option parsing----------------------
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-H", "--hash",
@@ -38,56 +21,18 @@ parser.add_argument("-t", "--threads",
                     type=int,
                     default=1,
                     help="Set the size of the threadpool")
+parser.add_argument("-q", "--quiet",
+                    action="store_true",
+                    help="Just outputs hash:plaintext pairs")
 
 args = parser.parse_args()
-print(args)
 
 
-#---------------------MAIN----------------------------
-
-print(machinery.SourceFileLoader("plugin_goog", ".").load_module())
-
-"""
-def testfunc(i):
-    time.sleep(1)
-    return "I'm {}".format(i)
+def worker(pname):
+    p = importlib.import_module("plugins.{}".format(pname))
+    return p
 
 with futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-    future_list = {executor.submit(testfunc, i):i for i in range(10)}
+    future_list = {executor.submit(worker, i):i for i in plugins.PLUGINS}
     for future in futures.as_completed(future_list):
         print(future_list[future], future.result())
-"""
-
-
-"""
-cleanup()
-
-#Load all plugins in the list
-plugins = load_plugins("plugins.list")
-
-print "\nInitilizing plugins...\n"
-
-for i in plugins:
-    i.init()
-
-if threadcount != 0:
-    print "\n\nCracking started (using threads)\n"
-    pool = threadpuul.ThreadPool(threadcount)
-    for i in plugins:
-        pool.add_task(i.run, crack, True)
-    pool.wait_completion()
-    print
-else:
-    print "\n\nCracking started\n"
-    result = None
-    for i in plugins:
-        result = i.run(crack)
-        if result != None:
-            print "Web: %s" % result[0]
-            print "Result: %s" % result[1]
-            break
-    if not result:
-        print "\nNo match found\n"
-    
-cleanup()
-"""
