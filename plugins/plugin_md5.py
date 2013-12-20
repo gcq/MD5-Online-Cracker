b572afd73fd62d30e70fa80358c6a62a8cc6430b
gcq <gcquiroga@gmail.com>
2013-12-20 03:05:52 +0100

    Worked md5

--- plugins/plugin_md5.py
+++ plugins/plugin_md5.py
@@ -1,19 +1,13 @@
-import mechanize
+import requests
+from bs4 import BeautifulSoup as bs
 
-def init():
-    print "[%s] enabled" % __name__.upper()
 
-def say(string):
-    print "[%s] %s" % (__name__.upper(), string)
-
-def run(string, thread=False):
-    br = mechanize.Browser()
-    br.open("http://www.md5.net/cracker.php")
-    br.select_form(nr=0)
-    br["hash"] = string
-    response = br.submit()
-    result = ["md5.net", response.readlines()[39].replace('<input type="text" id="hash" size="32" value="', "").replace('"/>', "").strip()]
-    if not result[1] == "Entry not found.":
-        if thread:
-            say(result)
-        return result
+def get_plaintext(h):
+    web = requests.post("http://www.md5.net/cracker.php",
+                        data={"hash":h}).text
+    soup = bs(web)
+    value = soup.find(id="hash")["value"]
+    if value != "Entry not found.":
+        return value
+    else:
+        return None
