9fddd191f777697a7495d9cb16be6fb3c0770db0
gcq <gcquiroga@gmail.com>
2013-12-20 03:06:08 +0100

    Worked md5-hash

--- plugins/plugin_md5-hash.py
+++ plugins/plugin_md5-hash.py
@@ -1,17 +1,11 @@
-import mechanize
+import requests
+from bs4 import BeautifulSoup as bs
 
-def init():
-	print "[%s] enabled" % __name__.upper()
 
-def say(string):
-    print "[%s] %s" % (__name__.upper(), string)
-
-def run(string, thread=False):
-	br = mechanize.Browser()
-	br = br.open("http://www.md5-hash.com/md5-hashing-decrypt/%s" % string)
-	for i in br.readlines():
-		if i.find("Decrypted text for MD5 hash") != -1:
-		    result = i.replace('<p class="fs-12">Decrypted text for MD5 hash <strong>' + string + '</strong> is <strong class="result">', "").replace("</strong></p>", "").strip()
-		    if thread:
-		        ["md5-hash.com", result]
-			return ["md5-hash.com", result]
+def get_plaintext(h):
+    web = requests.get("http://www.md5-hash.com/md5-hashing-decrypt/{}".format(h)).text
+    soup = bs(web)
+    try:
+        return soup.find("strong", class_="result").text
+    except AttributeError:
+        return None
